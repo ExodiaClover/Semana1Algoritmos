db9480646e95e51587b57f3b413db2d4cbdd0efa
Daniel <aquininel11@gmail.com>
2025-09-05 10:12:37 -0500

    Primeros commits

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar nombre
+        Console.Write("¿Cuál es tu nombre? ");
+        string nombre = Console.ReadLine();
+
+        // Solicitar carrera
+        Console.Write("¿Qué carrera estudias? ");
+        string carrera = Console.ReadLine();
+
+        // Mostrar la información
+        Console.WriteLine($"Hola {nombre}, estás estudiando {carrera}.");
+    }
+
+    {
+        Console.WriteLine("\")
+}
