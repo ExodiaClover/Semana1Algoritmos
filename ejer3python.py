f4936153aea0a9dcd53b886a3b18bcade17e7d14
Pieer <ralexrei@gmail.com>
2025-09-06 00:26:41 -0500

    tercer commiteeo

---
+++ ejer3python.py
@@ -0,0 +1,17 @@
+n1 = int(input("Ingresar un numero entero: "))
+n2 = int(input("Ingresar el segundo numero entero: "))
+
+# calculo
+suma = n1 + n2
+resta = n1 - n2
+multiplicacion = n1 * n2
+division = n1 / n2
+
+print(f"\nLa suma de los numeros es: {suma} ")
+print(f"\nLa resta de los numeros es: {resta} ")
+print(f"\nLa multiplicacion de los numeros es: {multiplicacion} ")
+print(f"\nLa division de los numeros es: {division} ")
+
+
+
+
---
+++ ejericio3c#.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominandoTodoC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma, resta, multiplicacion;
+                double division;
+
+            Console.WriteLine("Ingresar el primer numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresar el segundo numero");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // calculo
+            suma = num1 + num2;
+            resta = num1 - num2;
+            multiplicacion = num1 * num2;
+            division = num1 / num2;
+
+            Console.WriteLine($"\nLa suma de {num1} + {num2} es: {suma}");
+            Console.WriteLine($"La resta de {num1} - {num2} es: {resta}");
+            Console.WriteLine($"La multiplicación de {num1} * {num2} es: {multiplicacion}");
+            Console.WriteLine($"La division de {num1} / {num2} es: {division}");
+        }
+    }
+}
